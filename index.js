62e8628b44c45209f2af26172ab24d97fc395978
Min Khanthee <pkhanthee@gmail.com>
2022-06-26 08:34:27 +1200

    add sphere to all vocabulary

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let montagnaText = document.querySelector('#la-montagna-text')
 let casaSphere = document.querySelector('#la-casa')
 let casaText = document.querySelector('#la-casa-text')
 
+let cagnaSphere = document.querySelector('#la-cagna')
+let cagnaText = document.querySelector('#la-cagna-text')
+
+let barcaSphere = document.querySelector('#la-barca')
+let barcaText = document.querySelector('#la-barca-text')
+
 montagnaSphere.addEventListener('click', (e) => {
   montagnaSphere.setAttribute('color', 'green')
   montagnaText.object3D.visible = true
@@ -12,4 +18,14 @@ montagnaSphere.addEventListener('click', (e) => {
 casaSphere.addEventListener('click', (e) => {
   casaSphere.setAttribute('color', 'green')
   casaText.object3D.visible = true
+})
+
+cagnaSphere.addEventListener('click', (e) => {
+  cagnaSphere.setAttribute('color', 'green')
+  cagnaText.object3D.visible = true
+})
+
+barcaSphere.addEventListener('click', (e) => {
+  barcaSphere.setAttribute('color', 'green')
+  barcaText.object3D.visible = true
 })
