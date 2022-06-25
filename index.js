5d2df3e83c66eba192b019342920fe9ef581f7f2
Min Khanthee <pkhanthee@gmail.com>
2022-06-26 08:18:17 +1200

    add sphere for la casa 🔴

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let montagnaSphere = document.querySelector('#la-montagna')
 let montagnaText = document.querySelector('#la-montagna-text')
 
+let casaSphere = document.querySelector('#la-casa')
+let casaText = document.querySelector('#la-casa-text')
+
 montagnaSphere.addEventListener('click', (e) => {
   montagnaSphere.setAttribute('color', 'green')
   montagnaText.object3D.visible = true
+})
+
+casaSphere.addEventListener('click', (e) => {
+  casaSphere.setAttribute('color', 'green')
+  casaText.object3D.visible = true
 })
