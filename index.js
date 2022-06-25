47e1590632cbda0d54aa8448eded126a99448ed5
Min Khanthee <pkhanthee@gmail.com>
2022-06-26 07:24:10 +1200

    refactor js

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ let montagnaText = document.querySelector('#la-montagna-text')
 
 montagnaSphere.addEventListener('click', (e) => {
   montagnaSphere.setAttribute('color', 'green')
-  montagnaText.setAttribute('visible', true)
-  // montagnaSphere.addAttribute('visible', 'true')
-  // montagnaSphere.object3D.visible = true;
+  montagnaText.object3D.visible = true
 })
